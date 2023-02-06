689b93fe358ed9e408312f9c232a68441c7e1d69
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2023-02-05 22:46:08 -0300

    Update return type

--- chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
+++ chapter1/simple-application/src/main/java/com/huaylupo/spmia/ch01/Application.java
@@ -1,5 +1,7 @@
 package com.huaylupo.spmia.ch01;
 
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,42 +14,56 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(value="hello")
+@RequestMapping(value = "hello")
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
-	@GetMapping(value="/{firstName}")
-	public String helloGET( 
-			@PathVariable("firstName") String firstName,
-			@RequestParam("lastName") String lastName) {
-		return String.format("{\"message\":\"Hello %s %s\"}",firstName, lastName);
-	}
-	
-	@PostMapping
-	public String helloPOST( @RequestBody HelloRequest request) {
-		return String.format("{\"message\":\"Hello %s %s\"}",request.getFirstName(), request.getLastName());
-	}
+    @GetMapping(value = "/{firstName}")
+    public HelloResponse helloGET(
+            @PathVariable("firstName") String firstName,
+            @RequestParam("lastName") String lastName) {
+
+        return HelloResponse.builder()
+                .message(String.format("Hello %s %s", firstName, lastName))
+                .build();
+    }
+
+    @PostMapping
+    public HelloResponse helloPOST(@RequestBody HelloRequest request) {
+        return HelloResponse.builder()
+                .message(String.format("Hello %s %s", request.getFirstName(), request.getLastName()))
+                .build();
+    }
 }
 
-class HelloRequest{
-	
-	private String firstName;
-	private String lastName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+@Builder
+@Getter
+class HelloResponse {
+    private String message;
+}
+
+class HelloRequest {
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
 
